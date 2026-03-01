fe7d7295cf87f2415b5cf2d417ba47c123febaa1
TasyDev <tamayocontat@gmail.com>
2026-03-01 11:16:16 -0500

    Date Time Get Api

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+# Date Time Endpoints
+
+from datetime import datetime, date, timedelta
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-    
-@app.get("/movies")
-async def root():
-    return {"message": "Movies"}
+    return {
+        "date": date.today(),
+        "time": datetime.now().time(),
+        "datetime": datetime.now(),
+        "timedelta": timedelta(days=1)
+    }

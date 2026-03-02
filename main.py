57a97f59d45973181d4320209a80b282c22e3d80
TasyDev <tamayocontat@gmail.com>
2026-03-01 23:07:47 -0500

    Class input data validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Fist Steps
 
+from pydantic import BaseModel, EmailStr
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,22 +12,34 @@ app = FastAPI()
 
 # Date Time Endpoints
 
-from datetime import datetime
-from zoneinfo import ZoneInfo
+# from datetime import datetime
+# from zoneinfo import ZoneInfo
 
-currentTimezone = {
-    "EST": "America/New_York",
-    "PST": "America/Los_Angeles",
-    "GMT": "Europe/London",
-    "UTC": "UTC",
-    "IST": "Asia/Kolkata"
-}
+# currentTimezone = {
+#     "EST": "America/New_York",
+#     "PST": "America/Los_Angeles",
+#     "GMT": "Europe/London",
+#     "UTC": "UTC",
+#     "IST": "Asia/Kolkata"
+# }
 
-@app.get("/{iso_str}")
-async def root(iso_str: str):
-    iso = iso_str.upper()
-    timesome_str = currentTimezone.get(iso)
-    tz = ZoneInfo(timesome_str)
-    return {
-        "time": datetime.now(tz).isoformat(),
-    }
+# @app.get("/{iso_str}")
+# async def root(iso_str: str):
+#     iso = iso_str.upper()
+#     timesome_str = currentTimezone.get(iso)
+#     tz = ZoneInfo(timesome_str)
+#     return {
+#         "time": datetime.now(tz).isoformat(),
+#     }
+
+from typing import Optional
+
+class User(BaseModel):
+    name: str
+    email: EmailStr
+    age: Optional[int] = None
+
+@app.post("/user")
+async def create_user(user: User):
+    return user
+    
